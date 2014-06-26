e50ad0413b49bdfa8c4fa3f7afd1cdd951f7558c
Jonathan Seligsohn <jbseligsohn@gmail.com>
2014-06-26 11:36:56 -0400

    worked on all pages, tried to clean up things, just need to add in portfolio stuff

--- js/app.js
+++ js/app.js
@@ -8,7 +8,10 @@ $('#menu-toggle').click(function() {
 	$('.swipe').slideToggle(300);
 });
 
+
+//When window reaches bottom of page, slide open the initials element.
 $(window).scroll(function(){ 
+   $('#bottom-scroll').fadeOut();
    if ($(window).scrollTop() == ($(document).height() - $(window).height())) {
      $('.footer-initials').addClass('initials-hover');
    }
@@ -17,13 +20,42 @@ $(window).scroll(function(){
    }
 })
 
+//Return the hover functionality to the initials element.
 $('.footer-initials').hover(function() {
 	$(this).removeClass('initials-hover');
 	});
 
 
-
+//Clicking on arrow scrolls the page to the bottom.
 $('#bottom-scroll').click(function() {
-  $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-  $(this).toggleClass('fa-rotate-180');
+  $("html, body").animate({ scrollTop: $(document).height() }, 1200);
+  // $(this).toggleClass('fa-rotate-180');
+  $(this).fadeOut();
 })
+
+var $front_height = $('.front').css('height');
+$('.back').css('height', $front_height);
+
+$('.skill-icon-code').hover(function() {
+  $(this).toggleClass('stretching');
+});
+
+$('.skill-icon-software').hover(function() {
+  $(this).toggleClass('bouncing');
+});
+
+$('.skill-icon-tools').hover(function() {
+  $(this).toggleClass('expanding');
+});
+
+$('.skill-icon-database').hover(function() {
+  $(this).toggleClass('spinning');
+});
+
+
+
+
+
+
+
+
